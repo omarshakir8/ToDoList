a22ad936239b93ceed4ae621afd5a711d9d949e8
omarshakir8 <omarshakir8@gmail.com>
2024-06-01 03:15:28 -0400

    final edits and changes to addtodos along with persist data function

--- reactjs-ToDoList/src/App.jsx
+++ reactjs-ToDoList/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
@@ -7,39 +7,50 @@ function App() {
   const [todos, setTodos] = useState([])
   const [todoValue, setTodoValue] = useState('')
 
+  function persistData(newList) {
+    localStorage.setItem('todos', JSON.stringify({ todos: newList }))
+  }
 
+  function handleAddTodos(newTodo) {
+    const newTodoList = [...todos, newTodo]
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
 
+  function handleDeleteTodo(index) {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    persistData(newTodoList)
+    setTodos(newTodoList)
+  }
 
-function handleAddTodos(newTodo) {
-  const newTodoList = [...todos, newTodo]
-  setTodos(newTodoList)
-}
+  function handleEditTodo(index) {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodo(index)
+  }
 
+  useEffect(() => {
+    if (!localStorage) {
+      return
+    }
 
-function handleDeleteTodos(newTodo){
-  const newTodoList = [...todos, newTodo]
-  setTodos(newTodoList)
-}
+    let localTodos = localStorage.getItem('todos')
+    if (!localTodos) {
+      return
+    }
 
-function handleDeleteTodo(index) {
-  const newTodoList = todos.filter((todo, todoIndex) => {
-  return todoIndex !== index
-})
-setTodos(newTodoList)
-}
+    console.log(localTodos)
+    localTodos = JSON.parse(localTodos).todos
+    setTodos(localTodos)
 
-function handleEditTodo(index) {
-  const valueToBeEdited = todos[index]
-  setTodoValue(valueToBeEdited)
-  handleDeleteTodo(index)
-
-}
+  }, [])
 
   return (
     <>
       <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
-      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos= {todos} />
-      
+      <TodoList handleEditTodo={handleEditTodo} handleDeleteTodo={handleDeleteTodo} todos={todos} />
     </>
   )
 }
--- reactjs-ToDoList/src/components/TodoInput.jsx
+++ reactjs-ToDoList/src/components/TodoInput.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
 
-export default function TodoInput(props){
-    const { handleAddTodo, todoValue, setTodoValue } = props
+export default function TodoInput(props) {
+    const { handleAddTodos, todoValue, setTodoValue } = props
     return (
         <header>
-            <input value={todoValue} onChange= {(e) => {setTodoValue(e.target.value)}} placeholder="Enter To do........"/>
+            <input value={todoValue} onChange={(e) => {
+                setTodoValue(e.target.value)
+            }} placeholder="Enter todo..." />
             <button onClick={() => {
                 handleAddTodos(todoValue)
+                setTodoValue('')
             }}>Add</button>
         </header>
     )
